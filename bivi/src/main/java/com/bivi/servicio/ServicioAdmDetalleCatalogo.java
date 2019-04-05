486d4b576612fd4a4089d1f9cafb14eafd5c33a3
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2019-04-05 16:56:30 -0500

    Pequeño cambio

--- bivi/src/main/java/com/bivi/servicio/ServicioAdmDetalleCatalogo.java
+++ bivi/src/main/java/com/bivi/servicio/ServicioAdmDetalleCatalogo.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import com.bivi.modelo.*;
-import com.scl.administracion.modelo.AdmDetalleCatalogo;
 
 
 
